efc4ba5abc44863cdda262c95fe20ddcc264683f
mshajkarami <mshajkarami.dev@gmail.com>
2025-05-04 11:07:58 +0330

    feat: Implement Firebase Cloud Messaging Service
    This commit implements a new Firebase Cloud Messaging (FCM) service, `MyFirebaseMessagingService`, to handle incoming messages and manage notifications.
    
    **Key changes:**
    
    -   Created `MyFirebaseMessagingService.java`:
        -   Extends `FirebaseMessagingService` to handle FCM messages.
        -   Overrides `onMessageReceived` to log message data and notification details.
        -   Overrides `onNewToken` to handle token refresh.
        -   Implements `scheduleJob` to schedule a background work task using `WorkManager`.
        -   Implements `handleNow` for short-lived tasks.
        -   Implements `sendRegistrationToServer` for registering token with server.
        -   Implements `sendNotification` to create and display a notification.
        -   Uses `NotificationCompat.Builder` for notification creation.
        -   Handles notification channel creation for Android Oreo and above.
    - Log added for received message, data payload, Notification body and refreshed token.

--- app/src/main/java/ir/hajkarami/firebasecloudmessaging/MyFirebaseMessagingService.java
+++ app/src/main/java/ir/hajkarami/firebasecloudmessaging/MyFirebaseMessagingService.java
@@ -1,17 +1,84 @@
 package ir.hajkarami.firebasecloudmessaging;
 
-import android.app.Service;
-import android.content.Context;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
-import android.os.IBinder;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
 
-public class MyFirebaseMessagingService extends Service {
-    @Nullable
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String Tag = "MyFirebaseMessagingService";
+
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public void onMessageReceived(@NonNull RemoteMessage message) {
+        Log.v(Tag, "Received message from: " + message.getFrom());
+        if (message.getData().size() > 0) {
+            Log.v(Tag, "Data payload: " + message.getData());
+            if (true) {
+                scheduleJob();
+            } else {
+                handleNow();
+            }
+        }
+        // Check if message contains a notification payload.
+        if (message.getNotification() != null) {
+            Log.v(Tag, "Notification body: " + message.getNotification().getBody());
+        }
     }
-}
 
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        Log.v(Tag, "Refreshed token: " + token);
+        sendRegistrationToServer(token);
+    }
+
+
+    private void scheduleJob() {
+        // [START dispatch_job]
+        OneTimeWorkRequest work = new OneTimeWorkRequest.Builder(MyWorker.class).build();
+        WorkManager.getInstance(this).beginWith(work).enqueue();
+        // [END dispatch_job]
+    }
+
+    private void handleNow() {
+        Log.v(Tag, "Short lived task is done.");
+    }
+
+    private void sendRegistrationToServer(String token) {
+
+    }
+
+    private void sendNotification(String messageBody) {
+        Intent i = new Intent(this, MainActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
+        String chanelId = getString(R.string.app_name);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, chanelId)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Hello Welcome To Use this App")
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(chanelId, "Channel Human readable Title", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+        notificationManager.notify(0, builder.build());
+    }
+
+}
